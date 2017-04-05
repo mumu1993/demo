e2bb52865af89104f9fba3f6a2e1cbe303ecdb94
mumu <mtrabc@vip.qq.com>
2017-04-06 00:00:18 +0800

    add

--- src/components/itemcontainer.vue
+++ src/components/itemcontainer.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-      i am container
+      <header class="top-tips">
+        <span class="num-tip" v-if="fatherComponent == 'home'">{{level}}</span>
+        <span class="num-tip" v-if="fatherComponent == 'item'">题目{{itemNum}}</span>
+      </header>
+      <div v-if="fatherComponent == 'home'">
+        <router-link to="item" class="start button_style"></router-link>
+      </div>
+
     </div>
 </template>
 
 <script>
+
+ import { mapState, mapActions } from 'vuex'
 
 export default {
     name: 'itemcontainer',
@@ -12,13 +21,37 @@ export default {
     created(){
 
     },
+    props: ['fatherComponent'],
     data(){
+      return {
 
-    }
+      }
+    },
+    computed: mapState({
+      level: state => state.level,
+      itemNum: state => state.itemNum
+    })
 }
 
 </script>
 
 <style lang="less">
-
+  .to-item{
+    width: 10px;
+    height: 10px;
+  }
+  .button_style{
+    display: block;
+    height: 2.1rem;
+    width: 4.35rem;
+    background-size: 100% 100%;
+    position: absolute;
+    top: 16.5rem;
+    left: 50%;
+    margin-left: -2.4rem;
+    background-repeat: no-repeat;
+  }
+  .start{
+    background-image: url("../images/1-4.png");
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './config/rem'
-
+import store from './store/'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
